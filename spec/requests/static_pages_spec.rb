f206ad7391f84f61aaa8ac27c1f75898daa147d3
ansonwolfe <ansonwolfe@gmail.com>
2013-07-16 12:54:58 -0700

    end of chapter 3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,6 +14,13 @@ describe "Static pages" do
       page.should have_selector('title',
                         :text => "Ruby on Rails Tutorial Sample App | Home")
     end
+    
+    it "should have a non-blank body" do
+    	get 'home'
+    	response.body.should_not =~ /<body>\s*<\/body>/
+    	#check there is no whitespace / no information in the body tags
+    end	
+    
   end
 
   describe "Help page" do
@@ -43,4 +50,20 @@ describe "Static pages" do
                     :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
+  
+   describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end 
+
+
 end
