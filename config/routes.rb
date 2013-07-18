59436cdeae1f3591408041610004765e2f89e65e
ansonwolfe <ansonwolfe@gmail.com>
2013-07-18 11:41:46 -0700

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 SampleApp::Application.routes.draw do
-#   get "static_pages/home"
-# 
-#   get "static_pages/help"
-#   
-#   get "static_pages/about"
-#   
-#   get "static_pages/contact"
+  get "users/new"
 
+#   get "static_pages/home"
+#   replaced with root to: 'static_pages#home'
+
+#   get "static_pages/help"
+#   get "static_pages/about"
+#   get "static_pages/contact"
+#   replaced with the mat '/help', to:'static_pages#help' lines below
   root to: 'static_pages#home'
+  
+  match '/signup', to: 'users#new'
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
