9fdc0ee7a8ff6e0ce5aebf969cbb503de370e796
ansonwolfe <ansonwolfe@gmail.com>
2013-07-19 02:41:14 -0700

    User signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users
+  
 
 #   get "static_pages/home"
 #   replaced with root to: 'static_pages#home'
@@ -15,6 +16,8 @@ SampleApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  
+
 
 
   # The priority is based upon order of creation:
