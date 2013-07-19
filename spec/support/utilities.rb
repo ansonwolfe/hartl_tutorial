ea12c70297e55efe662782818789cf45de3badda
ansonwolfe <ansonwolfe@gmail.com>
2013-07-19 01:38:14 -0700

    Basic user model + passwords

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,11 @@
-def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+include ApplicationHelper
+
+#replaced by spec/helpers/application_helper_spec codes and the above line to include it
+# def full_title(page_title)
+#   base_title = "Ruby on Rails Tutorial Sample App"
+#   if page_title.empty?
+#     base_title
+#   else
+#     "#{base_title} | #{page_title}"
+#   end
+# end
